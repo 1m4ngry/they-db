d50cf7a7351b8fb4591c448189c3dd09c84c8849
Colin Watson <cjwatson@debian.org>
2004-06-27 23:21:30 +0000

    man-db: Sync to 2.4.2-2.
      * mandb only complains if it failed to do anything on create, not on
        update (closes: #211977).
      * Teach encodings layer about Galician, Indonesian, Croatian, and Slovak.

--- src/mandb.c
+++ src/mandb.c
@@ -627,7 +627,7 @@ int main (int argc, char *argv[])
 		chdir (cwd);
 #endif /* __profile__ */
 
-	if (!amount)
-		error (FAIL, 0, _("No databases updated."));
+	if (create && !amount)
+		error (FAIL, 0, _("No databases created."));
 	exit (OK);
 }
