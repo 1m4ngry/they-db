6b6ad11db32e4e0cfc4941f444bb596de48bbf09
Colin Watson <cjwatson@debian.org>
2014-01-11 02:16:49 +0000

    Import man-db_2.3.18.orig.tar.gz

--- autogen.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh -e
-
-aclocal
-autoheader && date > stamp-h.in
-autoconf
--- release.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh -e
-
-# Build automatically generated files
-./autogen.sh
-
-# Basic configure to get 'make dist'
-./configure
-
-make dist
